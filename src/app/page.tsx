b49ec86cf249530c52df2b9cce974f78f70f8b12
Vaibhav Bisht <56401767+meanOtaku@users.noreply.github.com>
2025-05-16 20:54:54 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function EmailExtractor() {
             const bodyData = {
                 emailList: emails,
             };
-            const result = await fetch("http://email-backend-taupe.vercel.app/email", {
+            const result = await fetch("https://email-backend-taupe.vercel.app/email", {
                 method: "POST",
                 headers: {
                     'Accept': 'application/json',
